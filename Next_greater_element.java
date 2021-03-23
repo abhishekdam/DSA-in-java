2a10823127610e052814f187f8ae78afcd2658d4
Abhishek Dam <abhishekdam852@gmail.com>
2021-03-23 20:42:15 +0530

    next greater element

---
+++ Next_greater_element.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class NGE {
+    public static int[] findNGE(int[] arr) {
+        int nge[] = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(arr.length - 1);
+        nge[arr.length - 1] = -1;
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            while (st.size() > 0 && st.peek() <= arr[i]) {
+                st.pop();
+            }
+            if (st.size() == 0) {
+                nge[i] = -1;
+            } else {
+                nge[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return nge;
+    }
+
+    public static void display(int nge[]) {
+        for (int j = 0; j < nge.length; j++) {
+            System.out.print(nge[j]);
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        int nge[] = findNGE(a);
+        display(nge);
+    }
+}
